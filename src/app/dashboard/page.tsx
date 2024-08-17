e5b439e606369f297127ebae7b169197d785f4cb
jay-2001 <90552695+jay-2001@users.noreply.github.com>
2024-08-18 00:46:57 +0530

    Auth Page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import Dashboard from "@/app/dashboard/dashboard"
+import SideNavbar from "@/components/SideNavbar"
+
+export default function DashboardPage() {
+  return (
+      <Dashboard />
+  );
+}

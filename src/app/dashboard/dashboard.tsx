0bcb205edd59119850102b3d54e82b230d65f394
Hrushi Patel <72564568+Hp-175@users.noreply.github.com>
2024-09-01 12:49:46 +0530

    Feature: Dashboard works with all filters.

--- src/app/dashboard/dashboard.tsx
+++ src/app/dashboard/dashboard.tsx
@@ -7,20 +7,21 @@ interface Filter {
 }
 
 interface Influencer {
-  name: string;
-  country: string;
-  platform: string[];
-  language: string;
-  followers: string;
-  growth: string;
-  topics: string[];
-  niche: string;
+  fullName: string;
+  username: string;
+  profilePicId: string;
+  totalFollowerCount: string;
+  categories: string[];
 }
 
 const Dashboard: React.FC = () => {
   const [locationFilter, setLocationFilter] = useState<Filter[]>([]);
   const [followerFilter, setFollowerFilter] = useState<Filter[]>([]);
   const [categoryFilter, setCategoryFilter] = useState<Filter[]>([]);
+
+  const [influencers, setInfluencers] = useState<Influencer[]>([]);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(0);
 
   // Temporary options
   const followerOptions = [
@@ -35,7 +36,19 @@ const Dashboard: React.FC = () => {
     ">1M",
   ];
 
-  const categoryOptions = ["Tech", "Food", "Lifestyle", "Travel"];
+  const categoryOptions = [
+    "Fashion", 
+    "Beauty", 
+    "Health", 
+    "Food", 
+    "Travel", 
+    "Lifestyle", 
+    "Automotive", 
+    "Art and Design", 
+    "Education", 
+    "Pets", 
+    "Home Decor"
+  ];
 
   const [locationSuggestions, setLocationSuggestions] = useState<string[]>([]);
   const [locationInput, setLocationInput] = useState<string>("");
@@ -43,7 +56,7 @@ const Dashboard: React.FC = () => {
   const debounceTimer = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    if (locationInput.length >= 3 && locationInput !== lastQuery) {
+    if (locationInput.length >= 1 && locationInput !== lastQuery) {
       // Clear any existing debounce timer
       if (debounceTimer.current) {
         clearTimeout(debounceTimer.current);
@@ -82,7 +95,7 @@ const Dashboard: React.FC = () => {
   const handleInputChange = (value: string) => {
     setLocationInput(value);
 
-    if (value.length < 3) {
+    if (value.length < 1) {
       // Clear suggestions if input is shorter than 3 characters
       setLocationSuggestions([]);
     }
@@ -104,9 +117,7 @@ const Dashboard: React.FC = () => {
 
   // Other handlers...
   const handleSelectLocation = (label: string) => {
-    if (!locationFilter.some((filter) => filter.label === label)) {
-      setLocationFilter([...locationFilter, { label }]);
-    }
+    setLocationFilter([{label}]);
     setLocationInput("");
     setLocationSuggestions([]);
   };
@@ -124,9 +135,7 @@ const Dashboard: React.FC = () => {
 
   // Handling selection of follower count
   const handleSelectFollower = (label: string) => {
-    if (!followerFilter.some((filter) => filter.label === label)) {
-      setFollowerFilter([...followerFilter, { label }]);
-    }
+    setFollowerFilter([{label}]);
   };
 
   // Handling selection of category
@@ -136,28 +145,72 @@ const Dashboard: React.FC = () => {
     }
   };
 
-  const influencers: Influencer[] = [
-    {
-      name: "krilovee",
-      country: "India",
-      platform: ["Instagram"],
-      language: "English",
-      followers: "1,260,000",
-      growth: "2.44%",
-      topics: ["Food", "Lifestyle", "Travel Guide"],
-      niche: "Shorts",
-    },
-    {
-      name: "Tech Logic in Telugu",
-      country: "India",
-      platform: ["Instagram"],
-      language: "English",
-      followers: "655,000",
-      growth: "0.61%",
-      topics: ["Apple", "Technology Design", "Gadget"],
-      niche: "Gadget",
-    },
-  ];
+  const handleSearch = async (page: number = 0) => {
+    setCurrentPage(page);
+
+    // Build query parameters
+    let queryParams = `page=${page}&size=10`;
+    if (locationFilter.length > 0) {
+      const locationToSearch = locationFilter[0].label.split(",");
+      if(locationToSearch.length == 3)
+      {
+        queryParams+="&city="+locationToSearch[0].toString().trimStart();
+        queryParams+="&state="+locationToSearch[1].toString().trimStart();
+        queryParams+="&country="+locationToSearch[2].toString().trimStart();
+      }
+      else if(locationToSearch.length == 2)
+      {
+        queryParams+="&state="+locationToSearch[0].toString().trimStart();
+        queryParams+="&country="+locationToSearch[1].toString().trimStart();
+      }
+      else if(locationToSearch.length == 1)
+      {
+        queryParams+="&country="+locationToSearch[0].toString().trimStart();
+      }
+    }
+    
+    if (followerFilter.length > 0) {
+      const [minFollowers, maxFollowers] = followerFilter[0].label.split("-").map((v) => v.replace(">", ""));
+      queryParams+="&minFollowers="+minFollowers;
+      if (maxFollowers) {
+        queryParams+="&maxFollowers="+maxFollowers;
+      }
+    }
+
+    if (categoryFilter.length > 0) {
+      queryParams+="&categoryNames="+categoryFilter.map((filter) => filter.label).join(",");
+    }
+    console.log(queryParams.toString());
+    try {
+      const response = await fetch(`http://localhost:8080/api/instagram/get/influencer?${queryParams.toString()}`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setInfluencers(data.content);
+        setTotalPages(data.totalPages);
+      } else {
+        console.error("Error fetching influencers");
+      }
+    } catch (error) {
+      console.error("Error fetching influencers", error);
+    }
+  };
+
+  const handlePageClick = (pageNumber: number) => {
+    handleSearch(pageNumber);
+  };
+
+  const trimText = (text: string, maxLength: number = 16): string => {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength - 3) + "...";
+    }
+    return text;
+  };
 
   return (
     <div className="p-8 bg-white text-gray-900">
@@ -166,29 +219,28 @@ const Dashboard: React.FC = () => {
       <div className="flex space-x-4 mb-4">
           {/* Location Input */}
           <div className="relative flex-grow">
-  <input
-    type="text"
-    value={locationInput}
-    onChange={(e) => handleInputChange(e.target.value)}
-    onBlur={handleBlur} // Handle when input loses focus
-    placeholder="Enter location"
-    className="w-full p-2 rounded-md bg-gray-200 border border-gray-300"
-  />
-  {locationSuggestions.length > 0 && (
-    <div className="absolute top-full left-0 right-0 bg-white border border-gray-300 mt-1 rounded-md z-10">
-      {locationSuggestions.map((suggestion, index) => (
-        <div
-          key={index}
-          className="p-2 cursor-pointer hover:bg-gray-100"
-          onMouseDown={() => handleSelectLocation(parseLocation(suggestion))} // Use onMouseDown
-        >
-          {parseLocation(suggestion)}
-        </div>
-      ))}
-    </div>
-  )}
-</div>
-
+            <input
+              type="text"
+              value={locationInput}
+              onChange={(e) => handleInputChange(e.target.value)}
+              onBlur={handleBlur} // Handle when input loses focus
+              placeholder="Enter location"
+              className="w-full p-2 rounded-md bg-gray-200 border border-gray-300"
+            />
+            {locationSuggestions.length > 0 && (
+              <div className="absolute top-full left-0 right-0 bg-white border border-gray-300 mt-1 rounded-md z-10">
+                {locationSuggestions.map((suggestion, index) => (
+                  <div
+                    key={index}
+                    className="p-2 cursor-pointer hover:bg-gray-100"
+                    onMouseDown={() => {handleSelectLocation(parseLocation(suggestion));}}
+                  >
+                    {parseLocation(suggestion)}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
           {/* Follower Count Dropdown */}
           <select
             className="w-40 p-2 rounded-md bg-gray-200 border border-gray-300"
@@ -253,7 +305,7 @@ const Dashboard: React.FC = () => {
         {/* Profile Count and Search inside Filters Card */}
         <div className="flex justify-between items-center">
           <span className="text-gray-600">228,814 users with profiles</span>
-          <button className="py-2 px-4 rounded-md bg-gray-800 text-white">Search</button>
+          <button onClick={() => handleSearch(0)} className="py-2 px-4 rounded-md bg-gray-800 text-white">Search</button>
         </div>
       </div>
 
@@ -262,24 +314,41 @@ const Dashboard: React.FC = () => {
         {influencers.map((influencer, index) => (
           <div key={index} className="p-4 bg-white rounded-lg shadow-all-sides flex justify-between items-center">
             <div>
-              <div className="text-xl font-bold">{influencer.name}</div>
-              <div className="text-sm text-gray-600">{influencer.country}</div>
+              <div className="text-l font-bold">{trimText(influencer.fullName)}</div>
+              <div className="text-sm text-gray-600">{trimText(influencer.username,18)}</div>
             </div>
-            <div>
-              <div className="text-right">{influencer.followers} followers</div>
-              <div className="text-green-500">{influencer.growth} growth</div>
+            <div className="follower-count">
+              <div className="text-right">{influencer.totalFollowerCount} followers</div>
+              {/* <div className="text-green-500">{influencer.growth} growth</div> */}
             </div>
             <div className="flex space-x-2">
-              {influencer.topics.map((topic, idx) => (
+              {influencer.categories.map((topic, idx) => (
                 <span key={idx} className="bg-gray-300 py-1 px-2 rounded-md text-sm">
                   {topic}
                 </span>
               ))}
             </div>
-            <div className="text-sm text-gray-600">{influencer.niche}</div>
+            {/* <div className="text-sm text-gray-600">{influencer.niche}</div> */}
           </div>
         ))}
       </div>
+      {totalPages > 1 && (
+        <div className="mt-6 flex justify-end">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index}
+              onClick={() => handlePageClick(index)}
+              className={`mx-1 px-3 py-1 rounded-md ${
+                currentPage === index
+                  ? "bg-blue-600 text-white"
+                  : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+              }`}
+            >
+              {index + 1}
+            </button>
+          ))}
+          </div>
+      )}
     </div>
   );
 };
